df16a4033f09229df4e92f6d61d429bffc0d147b
shoxruxnosirov <shoxruxfriend@gmail.com>
2024-11-11 13:30:03 +0500

    login signup errors

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Ip } from '@nestjs/common';
+import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcryptjs';
@@ -6,6 +6,7 @@ import { IAccount } from './interfaces/account.interface';
 import { ISean } from './interfaces/sean.interface';
 import { CreateAccountDto, CreateSeanDto } from './dto/account.dto';
 import { JwtService } from '@nestjs/jwt';
+import { throws } from 'assert';
 
 
 @Injectable()
@@ -26,27 +27,36 @@ export class UsersService {
         return account ? true : false; // agar foydalanuvchi topilsa, true qaytaradi
     }
 
-    async createAccount(createAccountDto: CreateAccountDto): Promise<ISean | Error> {
-        if(await this.checkIfEmailExists(createAccountDto.email)) {
-            console.log("if ichida");
-            return new Error("user is already registired");
-        }
-        const hashedPassword = await bcrypt.hash(createAccountDto.password, 10);
-        createAccountDto.password = hashedPassword;
-        const createAccount = await new this.accountModel(createAccountDto).save();
-        const token = await this.jwtService.sign({account: createAccountDto.email}); // id orqali token yaratish qanday buladi
-        const createSean = new this.seanModel({account_id: createAccount.id, token});
-        return createSean.save();        
-    };
-
-    async createChat(createAccountDto: CreateSeanDto): Promise<ISean | Error> {
-        const account = await this.accountModel.findOne({ email: createAccountDto.email }).exec();
-        if(account && await bcrypt.compare(createAccountDto.password, account.password)) {
-            const token = this.jwtService.sign({account: account.email});
-            const createSean = new this.seanModel({account_id: account.id, token});
+    async createAccount(createAccountDto: CreateAccountDto): Promise<ISean> {
+        try {
+            if(await this.checkIfEmailExists(createAccountDto.email)) {
+                throw new ConflictException ('User is already registired');
+            }
+            const hashedPassword = await bcrypt.hash(createAccountDto.password, 10);
+            createAccountDto.password = hashedPassword;
+            const createAccount = await new this.accountModel(createAccountDto).save();
+            const token = await this.jwtService.sign({account: createAccountDto.email}); // id orqali token yaratish qanday buladi
+            const createSean = new this.seanModel({account_id: createAccount.id, token});
             return createSean.save();
         } 
-        return new Error("email is not registired yet");
+        catch (err) {
+            throw new ConflictException(err!.message || "unknown error");
+        }    
+    };
+
+    async createChat(createAccountDto: CreateSeanDto): Promise<ISean> {
+        try {
+            const account = await this.accountModel.findOne({ email: createAccountDto.email }).exec();
+            if(account && await bcrypt.compare(createAccountDto.password, account.password)) {
+                const token = this.jwtService.sign({account: account.email});
+                const createSean = new this.seanModel({account_id: account.id, token});
+                return createSean.save();
+            } 
+            throw new UnauthorizedException ("email is not registired yet");
+        }
+        catch (err) {
+            throw new UnauthorizedException(err!.message || "unkown error");
+        }
     };
 
     //bu ustida qayta ishlash kerak
